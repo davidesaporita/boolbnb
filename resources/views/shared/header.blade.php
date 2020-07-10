c9d95274b13dd5faa21cc0ece1049d2f4510798f
Davide Saporita <davide.saporita@gmail.com>
2020-07-11 01:02:53 +0200

    Braintree basic integration

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -9,6 +9,9 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- includes the Braintree JS client SDK -->
+    <script src="https://js.braintreegateway.com/web/dropin/1.22.1/js/dropin.min.js"></script>
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
