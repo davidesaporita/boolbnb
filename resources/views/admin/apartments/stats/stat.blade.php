4faebdff2239af8cb7b5df5c3afb8ab846f5119b
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-07-14 15:33:58 +0200

    add style chart

--- resources/views/admin/apartments/stats/stat.blade.php
+++ resources/views/admin/apartments/stats/stat.blade.php
@@ -2,39 +2,61 @@
 
 @section('content')
 
-<canvas id="myChart" width="400" height="400"></canvas>
+<div class="container">
+    <div class="card">
+        <div class="card-body w-50">
+            <canvas id="myChart" width="400" height="400"></canvas>
+        </div>
+    </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 
 <script>
     var ctx = document.getElementById('myChart');
     var myChart = new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
-            labels: ['start','views'],
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [{
                 label: '# views',
-                data: ['0', '{{$apartment->views}}'],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+                backgroundColor: 'lightblue',
+                fill: true,
+                borderColor: '#000',
+                data: ['26', '8', '12', '24', '25', '18', '{{$apartment->views}}'],
             }]
         },
         options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
+				responsive: true,
+				title: {
+					display: true,
+					text: 'Visualizzazioni'
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				},
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
+				scales: {
+					x: {
+						display: true,
+						scaleLabel: {
+							display: true,
+							
+						}
+					},
+					y: {
+						display: true,
+						scaleLabel: {
+							display: true,
+							
+						}
+					}
+				}
+			}
     });
 </script>
 
