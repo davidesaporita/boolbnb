cab5edfd64297a2c804d9f3f3819c6c8373914b0
Davide Saporita <davide.saporita@gmail.com>
2020-07-15 15:06:14 +0200

    Work in progress

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,15 +1,37 @@
 var places = require('places.js');
 import L from 'leaflet'
 
+var latlng = {
+    lat: '45.493040',
+    lng: '7.802840'
+}
+
 var placesAutocomplete = places({
     appId: 'plUKJGFB9TS3',
     apiKey: '8311a9eeb21c1dc5b519abf85c0854c6',
     container: document.querySelector('#search'),
-    templates: {
-        value: function(suggestion) {
-            return suggestion.name;
-        }
-    }
 }).configure({
-    
-});
+    type: 'address',
+    hitsPerPage: 10,
+    aroundLatLng: lat + ',' + lng,
+    aroundRadius: 20000
+  });
+
+placesAutocomplete.on('change', (e) => {
+    e.suggestions
+})
+
+  placesAutocomplete.search().then(function(suggestions) {
+    if (!suggestions[0]) {
+      return;
+    }
+  
+    // var name = suggestions[0].name;
+    // var country = suggestions[0].country;
+    // var formattedCity = locale_names[0] + ', ' + country;
+  
+    // var infoElt = document.querySelector("#reverse-city");
+    // infoElt.value = formattedCity;
+
+    console.log(suggestions);
+  });
