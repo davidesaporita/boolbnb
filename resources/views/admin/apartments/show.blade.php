fbb73023179d09a4991c4e681e64e6edb4e2ba23
Davide Saporita <davide.saporita@gmail.com>
2020-07-16 13:21:26 +0200

    Minor fix on route name

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -93,7 +93,7 @@
         
         <div class="mt-4 sponsor-plan">
             <h2>Guarda le stat sull'appartamento</h2>
-            <a class="btn btn-sm btn-dark" href="{{ route('admin.apartments.stats.stat', ['apartment' => $apartment ]) }}">Statistiche</a>
+            <a class="btn btn-sm btn-dark" href="{{ route('admin.apartments.stats.index', ['apartment' => $apartment ]) }}">Statistiche</a>
         </div>
 
         <div id="show-map" style="height: 300px"></div>
