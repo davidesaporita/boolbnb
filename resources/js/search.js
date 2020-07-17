e16732e3da4d49d5f4c42ba9d4443d63b711f9a6
carminelentisco <carminelentisco@gmail.com>
2020-07-17 10:11:53 +0200

    Fix map

--- resources/js/search.js
+++ resources/js/search.js
@@ -79,23 +79,7 @@ $.ajax({
     apartmentContainer.append(html)
   }
 
-  ///////////////////////////////////////////////////////////////////////////////////
-
-  // let marker = L.marker([geoLat, geoLng]).addTo(search);
-  // let marker = L.marker([lat, lng]).addTo(showMap);
-
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2FybWlsZW50aXNjbyIsImEiOiJja2NjNnRmYjcwMXMyMnlwdXg0ZDYxM3JwIn0.Zg-CS3Rc5Krle5GllL7reQ', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox/streets-v11',
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: 'your.mapbox.access.token'
-  }).addTo(search);
-
-  ///////////////////////////////////////////////////////////////////////////////////
-
-  
+  let marker = L.marker([lat, lng]).addTo(showMap);
 
 }).fail(function() {
 
