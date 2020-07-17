ed72784b34dd23b6604a0be2a91ca7e630a5c150
Emanuele Sanquirico <manu.sanquirico@gmail.com>
2020-07-17 12:10:50 +0200

    Dashboard basic + Dropdown route update

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,29 @@
 @include('shared.header')
 
+<div class="container">
 <main class="py-4">
+    <nav class=" navbar-light bg-light">
+        <ul class="nav nav-tabs nav-fill">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+              </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Statistics</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Inbox</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Calendar</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Your profile</a>
+              </li>
+          </ul>
+      </nav>
     <div class="container text-center">
         <a class="btn btn-lg btn-primary w-50 mt-5" href="{{ route('admin.apartments.create') }}">
             Add a new apartment
@@ -11,6 +34,7 @@
     </div>
     
 </main>
+</div>
 
 
 @include('shared.footer')
