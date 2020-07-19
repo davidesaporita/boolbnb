ef7ad3d94e64e604e5d65859f812d2ba858ab925
Davide Saporita <davide.saporita@gmail.com>
2020-07-19 19:02:16 +0200

    Fix query

--- app/Http/Controllers/Api/StatsController.php
+++ app/Http/Controllers/Api/StatsController.php
@@ -15,17 +15,17 @@ class StatsController extends Controller
 
         $apartment = Apartment::find($request->input('apartment_id'));
 
-        $views = Stat::whereApartmentId($apartment->id)
-                      ->select( array(
-                        \DB::raw('DATE(created_at) as date'),
-                        \DB::raw('sum(stat_type_id = "1") as views'),
-                        \DB::raw('sum(stat_type_id = "2") as info_requests'),
-                        \DB::raw('sum(stat_type_id = "3") as reviews')
-                      ))
-                      ->groupBy('date')
-                      ->orderBy('date')
-                      ->get();
+        $stats = Stat::whereApartmentId($apartment->id)
+                     ->select( array(
+                       \DB::raw('DATE(created_at) as date'),
+                       \DB::raw('sum(stat_type_id = "1") as views'),
+                       \DB::raw('sum(stat_type_id = "2") as info_requests'),
+                       \DB::raw('sum(stat_type_id = "3") as reviews')
+                     ))
+                     ->groupBy('date')
+                     ->orderBy('date')
+                     ->get();
 
-        return response()->json($views);
+        return response()->json($stats);
     }
 }
