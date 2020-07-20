accff939f43dead0a6ac8c5584442d16ce4fe0e0
Davide Saporita <davide.saporita@gmail.com>
2020-07-20 17:30:58 +0200

    closes #110 | Fixed paypal payment

---
+++ config/braintree.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'environment' => getenv('BT_ENVIRONMENT'),
+    'merchantId' => getenv('BT_MERCHANT_ID'),
+    'publicKey' => getenv('BT_PUBLIC_KEY'),
+    'privateKey' => getenv('BT_PRIVATE_KEY')
+];
