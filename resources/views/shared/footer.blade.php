8c4ed31c3e2c29fa5fd5ec7c6131f014acdd132c
carminelentisco <carminelentisco@gmail.com>
2020-07-21 20:21:03 +0200

    Nav-bar Complete

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,3 +1,8 @@
+
+    {{-- SCRIPT --}}
+    <script src="https://js.braintreegateway.com/web/dropin/1.22.1/js/dropin.min.js"></script> 
+    {{-- BRAINTREE --}}
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
 
--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -3,90 +3,77 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- includes the Braintree JS client SDK -->
-    <script src="https://js.braintreegateway.com/web/dropin/1.22.1/js/dropin.min.js"></script>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
+    {{-- Link --}}
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-
-    <!-- FontAwesome -->
-    <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-    <!-- Mappa -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/leaflet/1/leaflet.css" />
-
-    <!-- Styles -->
+    <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">      {{-- FontAwesome --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/leaflet/1/leaflet.css" />     {{-- Leaflet --}}
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        
+        <nav class="main-navbar">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    BoolBnB
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <div class="wrap-nav">
+                    {{-- Logo --}}
+                    <a href="{{ url('/') }}">
+                        <img src="{{URL::to('/')}}/img/logo-boolbnb.png" alt="">
+                    </a>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    @guest
+                        <div class="nav-action-guest">
+                            <a href="{{ route('login') }}">Login</a>
+                            <a href="{{ route('register') }}">Diventa Host</a>
+                        </div>
+                    @else
+                        {{-- Admin --}}
 
-                    </ul>
+                        <div class="nav-action-admin">
+                            <a class="nav-link mobile-user" data-toggle="dropdown">
+                                {{ Auth::user()->first_name }} {{ Auth::user()->last_name }} <span class="caret"></span>
+                            </a>
+                            <i id="menu-icon" class="menu-mobile fas fa-bars"></i>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->first_name }} {{ Auth::user()->last_name }} <span class="caret"></span>
-                                </a>
+                            <div class="menu-large">
+                                <ul>
+                                    <li><a href="{{ route('admin.index') }}">La tua Dashborad</a></li>
+                                    <li><a href=""><a href="{{ route('admin.apartments.create') }}">Aggiungi appartamento</a></a></li>
+                                    <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" >Logout</a></li>
+                                    <li>
+                                        <a class="nav-link desktop-user" data-toggle="dropdown">
+                                            {{ Auth::user()->first_name }} {{ Auth::user()->last_name }} <span class="caret"></span>
+                                        </a>
+                                    </li>
+                                </ul>
+                                
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </div>
 
+                        
+                        
+                    @endguest
+                </div>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('admin.index') }}">
-                                        Your Dashboard
-                                     </a>
-                                    <a class="dropdown-item" href="{{ route('admin.apartments.create') }}">
-                                        Add a new apartment
-                                     </a>
-                                    <a class="dropdown-item" href="{{ route('admin.apartments.index') }}">
-                                        Your apartments
-                                     </a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                <div id="sub-menu" class="sub-menu hidden">
+                    <div>
+                        <a href="{{ route('admin.index') }}">La tua Dashborad</a>
+                    </div>
+                    <div>
+                        <a href="{{ route('admin.apartments.create') }}">Aggiungi appartamento</a>
+                    </div>
+                    <div>
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" >Logout</a>
+                    </div>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </div>
             </div>
         </nav>
