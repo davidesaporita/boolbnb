669e04b08983e1ec769a0e06b7e6096bc9ee4eb7
carminelentisco <carminelentisco@gmail.com>
2020-07-21 13:14:45 +0200

    Struttura Base scss

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -17,7 +17,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- FontAwesome -->
     <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
