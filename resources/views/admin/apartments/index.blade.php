458c36a5b8d5feb0cace50d66652018508ee4e52
Davide Saporita <davide.saporita@gmail.com>
2020-07-24 18:03:22 +0200

    Fix data in frontend

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,18 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container d-flex flex-wrap justify-content-center">
-    <div class="mb-3 d-flex flex-wrap justify-content-around w-100">
-        <h3>Messaggi ricevuti: {{$messages_number}}</h3>
-        @if ($numvotes == 0)
-            <h3>Non ci sono recensioni!</h3>
-        @else    
-            <h3><i class="fas fa-star"></i>{{$average}}/5 ({{$numvotes}} {{$numvotes == 1 ? 'recensione' : 'recensioni'}})</h3>
-        @endif
-        <a class="btn btn-lg btn-danger" href="{{route('admin.apartments.create')}}">Aggiungi un appartamento</a>
-    </div>
+<div class="container d-flex flex-wrap justify-content-center" style="padding-top: 80px;">
     @foreach ($apartments as $apartment) 
         <div class="card mb-4 mr-3" style="width: 22rem;">
+            
+            @if($apartment->active == 0)
+                <h4 class="position-absolute" style="right:0px;">
+                    <span class="badge badge-dark p-2 m-2">Annuncio disabilitato</span>
+                </h4>
+            @endif
+
             @forelse ($apartment->sponsor_plans as $plan)
                 @if ($plan->sponsorships->deadline > $now)
                     <h4 class="position-absolute">
@@ -29,17 +27,13 @@
                     <a  href="{{route('admin.apartments.sponsorship.pay', ['apartment' => $apartment])}}" class="badge badge-secondary p-2 m-2">Sponsorizza!</a>
                 </h4>
             @endforelse
-            <?php 
-            $count = 0;
-            foreach ($apartment->messages as $message) {
-                $count++;
-            }
-            if ($count != 0) {
-                echo '<h3 class="position-absolute mt-5 ml-2"><span class="badge badge-danger mt-2">'; 
-                echo $count;
-                echo '</span></h3>';
-            }
-            ?>
+
+            @if(count($apartment->messages) > 0)
+                <h3 class="position-absolute mt-5 ml-2">
+                    <span class="badge badge-danger mt-2">{{ count($apartment->messages) }}</span>
+                </h3>
+            @endif
+
             <a href="{{ route('admin.apartments.show', $apartment->id)}}" class="text-decoration-none">
                 <img class="w-100 card-img-top" style="height: 350px; object-fit: cover;" src="{{strpos($apartment->featured_img, '://') ? $apartment->featured_img : asset("/storage/" . $apartment->featured_img)}}" alt="{{$apartment->title}}">
             </a>
@@ -64,18 +58,19 @@
                         @forelse ($apartment->services as $service)
                             <span class="badge badge-pill badge-primary mb-2">{{$service->name}}</span>
                         @empty
-                            <p>Non ci sono servizi aggiuntivi!</p>
+                            <p><i>Nessuno<i></p>
                         @endforelse
                     </div>
                 </div>      
                 {{-- BUTTONS SHOW, DISABLED, DELETE --}}
                 <div class="mt-3 d-flex button-options">
-                    <a href="{{ route('admin.apartments.show', $apartment->id) }}" class="btn btn-sm btn-primary mr-2">Dettagli</a>
+                    <a href="{{ route('admin.apartments.show', $apartment->id) }}" class="btn btn-sm btn-info mr-2">Dettagli</a>
+                    <a href="{{ route('admin.apartments.edit', $apartment->id) }}" class="btn btn-sm btn-warning mr-2">Modifica</a>
                     <form action="{{ route('admin.apartments.toggle', $apartment->id) }}" method="POST">
                         @csrf
                         @method('PATCH')
                         @if($apartment->active == 1)
-                            <input type="submit" class="btn btn-sm btn-dark mr-2" value="Disabilita">
+                            <input type="submit" class="btn btn-sm btn-primary mr-2" value="Disabilita">
                         @else 
                             <input type="submit" class="btn btn-sm btn-success mr-2" value="Abilita">
                         @endif
@@ -91,6 +86,6 @@
     @endforeach
 </div>
 <div class="wrap-pagination mt-5 d-flex justify-content-center">
-    {{$apartments->links()}}
+    {{ $apartments->links() }}
 </div>
 @endsection
