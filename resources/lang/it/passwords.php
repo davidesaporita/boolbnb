3f6ac6233efb3d687673246a91258ba7c8c9a62b
Davide Saporita <davide.saporita@gmail.com>
2020-07-24 18:32:05 +0200

    closes #184 | Set italian language

---
+++ resources/lang/it/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'La tua password è stata resettata!',
+    'sent' => 'Ti abbiamo inviato via email il link per resettare la password!',
+    'throttled' => 'Attendi prima di riprovare.',
+    'token' => 'Il token per il reset della password non è vuoto.',
+    'user' => "Non esiste un utente con quell'indirizzo email.",
+
+];
