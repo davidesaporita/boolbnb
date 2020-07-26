df309523e4677926ce4e64d712225af857715edb
Davide Saporita <davide.saporita@gmail.com>
2020-07-27 01:54:35 +0200

    Fixed cards & labels

--- resources/views/shared/handlebars/template-card-home.blade.php
+++ resources/views/shared/handlebars/template-card-home.blade.php
@@ -1,9 +1,12 @@
 <script id="template-card-home" type="text/x-handlebars-template"> 
 
 <div class="card-search card-search-mobile">
-    <h4 class="position-absolute">
-          <span class="badge badge-success p-2 m-2">@{{ sponsored }}</span>
-    </h4>
+    @{{#if sponsored}}
+      <div class="plate-guest">
+        <span>@{{ sponsored }}</span>
+      </div>
+    @{{/if}}
+    
     <a href="{{ url('apartments') }}/@{{apartmentID}}">
       <img class="card-img-top" src="@{{ image }}" alt="@{{altImage }}">
     </a>
@@ -11,7 +14,7 @@
       <a href="{{ url('apartments') }}/@{{apartmentID}}">
         <h4 class="card-title">@{{ title }}</h4>
       </a>
-      <h5 class="card-title">@{{ apartmentCity }}, @{{ apartmentRegion }}</h5>
+      <h5 class="card-title">@{{ apartmentCity }}</h5>
       <span  style="display: block">@{{ apartmentAddress }}</span>
       <label for="distance">Distanza: </label>
       <span id="distance">@{{ distance }} Km</span> 
@@ -22,12 +25,17 @@
   <div class="card mb-3 card-desktop-view">
     <div class="row no-gutters card-desktop">
         <div class="col-md-4 img-container">
+          @{{#if sponsored}}
+          <div class="plate-guest">
+            <span>@{{ sponsored }}</span>
+          </div>
+        @{{/if}}
           <img src="@{{ image }}" alt="@{{altImage }}" class="card-img" alt="@{{altImage }}">
         </div>
         <div class="col-md-8">
           <div class="card-body">
             <h4 class="card-title">@{{ title }}</h4>
-            <h5 class="card-title">@{{ apartmentCity }}, @{{ apartmentRegion }}</h5>
+            <h5 class="card-title">@{{ apartmentCity }}</h5>
             <span  style="display: block">@{{ apartmentAddress }}</span>
             <label for="distance">Distanza: </label>
             <span id="distance">@{{ distance }} Km</span> 
